4617c9f0e6ef4a977b0a44b3100d96dd9ab2c381
mega-hertz91 <mega-hertz@bk.ru>
2018-05-02 17:46:18 +0500

    Добавлет блок живой пример

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,9 @@
   navList = document.querySelector(".main-nav__list");
   toggleButton = document.querySelector(".main-nav__button");
 
+  if(mainNav.classList.contains("main-nav--closed")) {
+    mainNav.classList.add("main-nav--open");
+  }
 
   toggleButton.addEventListener("click", function(evt){
     evt.preventDefault();
