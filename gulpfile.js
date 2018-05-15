de1fb8d4d3828a0606b70b1ee816fef2fc7b69c3
mega-hertz91 <mega-hertz@bk.ru>
2018-05-15 19:05:40 +0400

    Ошибка сборки спрайта свг

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-var mininfy = require ("gulp-csso");
+var minify = require ("gulp-csso");
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
@@ -20,11 +20,11 @@ gulp.task("style", function() {
       autoprefixer()
     ]))
     .pipe(gulp.dest("source/css"))
-    .pipe (mininfy())
+    .pipe(minify())
+    .pipe(rename(style.min.css))
     .pipe(gulp.dest("source/css"))
-    .pipe(rename("style.min.css"))
     .pipe(server.stream());
-});
+  });
 
 gulp.task("serve", ["style"], function() {
   server.init({
@@ -36,30 +36,30 @@ gulp.task("serve", ["style"], function() {
   });
 
 gulp.task("images", function () {
-   return gulp.src("source/img/**/*.{png,jpg,svg}")
-     .pipe(imagemin([
-       imagemin.optipng({optimizationLevel: 3}),
-       imagemin.jpegtran({progressive: true}),
-       imagemin.svgo()
-     ]))
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+   .pipe(imagemin([
+     imagemin.optipng({optimizationLevel: 3}),
+     imagemin.jpegtran({progressive: true}),
+     imagemin.svgo()
+   ]))
      .pipe(gulp.dest("source/img"));
-});
+   });
 
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*{png,jpg}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
-});
+  });
 
-gulp.task ("sprite", function () {
+gulp.task("sprite", function () {
   return gulp.src("source/img/icon-*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("source/img"));
-});
+  });
 
   gulp.watch("source/less/**/*.less", ["style"]);
   gulp.watch("source/*.html").on("change", server.reload);
-});
+  });
